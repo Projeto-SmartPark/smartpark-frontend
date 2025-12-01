9a7995d4ecbfdb21093c3a30efe1a8ca1b8372ce
Bruno Ricardo de Menezes <brunomenezes1511@gmail.com>
2025-12-01 00:26:10 -0300

    fix: local e deploy

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 // export const AUTH_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:9000/api';
 // export const BACKEND_API_URL = process.env.REACT_APP_API_BACKEND_URL || 'http://localhost:8000/api';
 
-export const AUTH_API_URL = process.env.REACT_APP_API_URL || 'https://smartpark-auth-production.up.railway.app/api';
-export const BACKEND_API_URL = process.env.REACT_APP_API_BACKEND_URL || 'https://smartpark-backend-production.up.railway.app/api';
+export const AUTH_API_URL = process.env.REACT_APP_API_URL;
+export const BACKEND_API_URL = process.env.REACT_APP_API_BACKEND_URL;
